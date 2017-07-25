84a81c4d085f32ddcfeef30782536694d05af42a
Megan Reardon <meganr@gmail.com>
2017-07-24 17:42:28 -0700

    adds CSS animations on scroll

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -39,4 +39,31 @@ $(document).ready(function() {
     });
   });
 
+  /* animations on scroll */
+  /* uses https://daneden.github.io/animate.css/ */
+  $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+
+  $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeInUp');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').addClass('animated pulse');
+  }, {
+    offset: '50%'
+  });
+
 });
