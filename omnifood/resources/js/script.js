c66118bfe85034b8b2c60f4d6ab6391a5c4c082d
Megan Reardon <meganr@gmail.com>
2017-07-24 18:09:56 -0700

    adds mobile navigation open and close

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -47,7 +47,6 @@ $(document).ready(function() {
     offset: '50%'
   });
 
-
   $('.js--wp-2').waypoint(function(direction) {
     $('.js--wp-2').addClass('animated fadeInUp');
   }, {
@@ -64,6 +63,20 @@ $(document).ready(function() {
     $('.js--wp-4').addClass('animated pulse');
   }, {
     offset: '50%'
+  });
+
+  /* mobile navigation */
+  $('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+    nav.slideToggle(200);
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
   });
 
 });
