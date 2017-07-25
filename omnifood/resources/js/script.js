d938db5bb169d082e7e829b010fd263bf441f834
Megan Reardon <meganr@gmail.com>
2017-07-24 17:19:26 -0700

    adds scrolling to sections from top nav

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
   /* for the sticky navigation */
+  /* uses the Waypoint jquery code from http://imakewebthings.com/waypoints/guides/jquery-zepto/ */
   $('.js--section-features').waypoint(function(direction) {
     if (direction == "down") {
       $('nav').addClass('sticky');
@@ -11,13 +12,31 @@ $(document).ready(function() {
     offset: '60px;'
   });
 
-  /* scroll on buttons */
+  /* scroll on button clicks */
   $('.js--scroll-to-plans').click(function () {
     $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000)
   });
 
   $('.js--scroll-to-start').click(function () {
     $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000)
+  });
+
+  /* smooth scrolling */
+  /* uses and older version of the code from https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          /* TODO return to fix animation not working correctly */
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
   });
 
 });
